4bd059121a0d30ac4726083036abecd19493e70d
amolthakare <100188653+amolthakare@users.noreply.github.com>
2023-05-11 15:35:03 +0530

    login signup part has been added

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+require('dotenv').config();
+// const cors = require("cors");
+const { connection } = require("./config/db");
+const routes = require("./routes/user.route");
+const { authenticate } = require("./middleware/authenticate.middleware");
+
+
+
+
+const app = express();
+app.use(express.json());
+
+// app.use(cors({origin:"*"}));
+// app.use("/",(err,res)=>{
+//     res.send("welcome to the nike backend");
+//     // console.log("welcome to the nike backend");
+// })
+
+app.use("/",routes)
+app.use(authenticate)
+app.listen(process.env.port,async()=>{
+    try{
+        await connection
+        console.log("connected to mongo");
+    }
+    catch(err){
+        console.log("msg:",err);
+    }
+    console.log(`connected to port ${process.env.port} successfully`)
+})
